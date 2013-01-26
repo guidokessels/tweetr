f4e6826f9c07b1f3ed1e29bd1f08bac9aad04824
Guido Kessels <guido.kessels@spilgames.com>
2013-01-26 14:49:20 +0100

    Ran GruntJS to generate dist folder and files

---
+++ dist/tweetr.js
@@ -0,0 +1,11 @@
+/*! tweetr - v0.1.0 - 2013-01-26
+* https://github.com/guidokessels/tweetr
+* Copyright (c) 2013 Guido Kessels; Licensed MIT */
+
+(function(exports) {
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
